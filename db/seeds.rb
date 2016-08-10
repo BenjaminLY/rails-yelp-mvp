ab4bb0cfb81910721fac2c102fcb47b0cfad6f4d
Benjamin Lyphoudt <benjamin.lyphoudt@gmail.com>
2016-08-10 13:16:45 +0200

    creation models controllers and db schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Review.destroy_all
+
+restaurants_attr = [
+  {
+    name: "Geoffroy l'Olive",
+    address: "3 Rue Geoffroy l'Angevin, 75004 Paris",
+    phone_number: "01 42 77 44 05",
+    category: "italian"
+  },
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré, 75008 Paris",
+    phone_number: "01 44 55 66 77",
+    category:     "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville, 75005 Paris",
+    phone_number: "01 02 03 04 05",
+    category:     "french"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes, 75016 Paris",
+    phone_number: "01 88 77 66 55",
+    category:     "belgian"
+  },
+  {
+    name:         "Asian Paris",
+    address:      "30 Avenue George V, 75008 Paris",
+    phone_number: "01 56 89 11 00",
+    category:     "japanese"
+  }
+]
+
+restaurants_attr.each { |params| Restaurant.create!(params) }
